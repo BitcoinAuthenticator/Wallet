486035f3a38e7b48e16e92bbdccd0677cfa1affe
Alon Muroch <alonmuroch@gmail.com>
2014-05-18 13:38:51 +0200

    added WalletFile locking for file access, still not fully tested

--- src/main/java/authenticator/WalletFile.java
+++ src/main/java/authenticator/WalletFile.java
@@ -9,12 +9,15 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.concurrent.locks.ReentrantLock;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+
+import com.google.bitcoin.utils.Threading;
 
 import authenticator.db.KeyObject;
 import authenticator.db.KeysArray;
@@ -41,14 +44,19 @@ import authenticator.db.PairingObject;
 public class WalletFile extends BASE{
 	
 	String filePath = null;
+	protected final ReentrantLock lock = Threading.lock("BAwalletfile");
 
 	/**Contructor defines the loclation of the .json file*/
 	public WalletFile(){
 		super(WalletFile.class);
+		lock.lock();
 		try {
 			filePath = BAUtils.getAbsolutePathForFile("wallet.json");//new java.io.File( "." ).getCanonicalPath() + "/wallet.json";
 		} catch (IOException e) {
 			e.printStackTrace();
+		}
+		finally{
+			lock.unlock();
 		}
 	}
 	
@@ -63,6 +71,7 @@ public class WalletFile extends BASE{
 		JSONArray jsonArr = new JSONArray();
 		for(PairingObject o:oPairing)
 			jsonArr.add(o.getJSONObject());
+		lock.lock();
 		try {
 			FileWriter file = new FileWriter(filePath);
 			file.write(jsonArr.toString());
@@ -71,48 +80,7 @@ public class WalletFile extends BASE{
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		/*JSONParser parser = new JSONParser();
-		Object obj = null;
-		try {
-			obj = parser.parse(new FileReader(filePath));
-		} catch (IOException | ParseException e) {
-			e.printStackTrace();
-		}
-		JSONObject jsonObject = (JSONObject) obj;
-		String aeskey = (String) jsonObject.get("aes_key");
-		String mpubkey = (String) jsonObject.get("master_public_key");
-		String chaincode = (String) jsonObject.get("chain_code");
-		long numkeys = (Long) jsonObject.get("keys_n");
-		JSONArray msg = (JSONArray) jsonObject.get("keys");
-		Iterator<JSONObject> iterator = msg.iterator();
-		JSONArray jsonlist = new JSONArray();
-		while (iterator.hasNext()) {
-			jsonlist.add(iterator.next());
-		}		
-		//Save the new json file
-		Map newobj=new LinkedHashMap();
-		newobj.put("aes_key", aeskey);
-		newobj.put("master_public_key", mpubkey);
-		newobj.put("chain_code", chaincode);
-		newobj.put("testnet", testnet);
-		newobj.put("keys_n", numkeys);
-		newobj.put("keys", jsonlist);
-		StringWriter jsonOut = new StringWriter();
-		try {
-			JSONValue.writeJSONString(newobj, jsonOut);
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-		String jsonText = jsonOut.toString();
-		try {
-			FileWriter file = new FileWriter(filePath);
-			file.write(jsonText);
-			file.flush();
-			file.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}*/
+		finally{ lock.unlock(); }
 	}
 	
 	/**
@@ -141,6 +109,7 @@ public class WalletFile extends BASE{
 		JSONArray jsonArr = new JSONArray();
 		for(PairingObject o:oPairing)
 			jsonArr.add(o.getJSONObject());
+		lock.lock();
 		try {
 			FileWriter file = new FileWriter(filePath);
 			file.write(jsonArr.toString());
@@ -149,58 +118,7 @@ public class WalletFile extends BASE{
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		/*//Load the existing json file
-		JSONParser parser = new JSONParser();
-		Object obj = null;
-		try {
-			obj = parser.parse(new FileReader(filePath));
-		} catch (IOException | ParseException e) {
-			e.printStackTrace();
-		}
-		JSONObject jsonObject = (JSONObject) obj;
-		String aeskey = (String) jsonObject.get("aes_key");
-		String mpubkey = (String) jsonObject.get("master_public_key");
-		String chaincode = (String) jsonObject.get("chain_code");
-		Boolean testnet = (Boolean) jsonObject.get("testnet");
-		long numkeys = (Long) jsonObject.get("keys_n");
-		JSONArray msg = (JSONArray) jsonObject.get("keys");
-		Iterator<JSONObject> iterator = msg.iterator();
-		JSONArray jsonlist = new JSONArray();
-		while (iterator.hasNext()) {
-			jsonlist.add(iterator.next());
-		}
-		//Create the key json object
-		numkeys ++;
-		JSONObject keyobj = new JSONObject();
-		keyobj.put("index",new Integer((int) numkeys));
-		keyobj.put("priv_key", privkey);
-		keyobj.put("address", addr);
-		//Add key object to array
-		jsonlist.add(keyobj);
-		//Save the new json file
-		Map newobj=new LinkedHashMap();
-		newobj.put("aes_key", aeskey);
-		newobj.put("master_public_key", mpubkey);
-		newobj.put("chain_code", chaincode);
-		newobj.put("testnet", testnet);
-		newobj.put("keys_n", numkeys);
-		newobj.put("keys", jsonlist);
-		StringWriter jsonOut = new StringWriter();
-		try {
-			JSONValue.writeJSONString(newobj, jsonOut);
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-		String jsonText = jsonOut.toString();
-			try {
-				FileWriter file = new FileWriter(filePath);
-				file.write(jsonText);
-				file.flush();
-				file.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}*/
+		finally { lock.unlock(); }
 	}
 	
 	/**This method is used during pairing. It saves the data from the Autheticator to file*/
@@ -221,6 +139,7 @@ public class WalletFile extends BASE{
 		JSONParser parser = new JSONParser();
 		Object obj = null;
 		JSONArray jsonArr = null;
+		lock.lock();
 		try {
 			try
 			{
@@ -236,45 +155,7 @@ public class WalletFile extends BASE{
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		
-		/*JSONArray jsonlist = new JSONArray();
-		Map obj=new LinkedHashMap();
-		obj.put("aes_key", key);
-		obj.put("master_public_key", mpubkey);
-		obj.put("chain_code", chaincode);
-		obj.put("GCM", GCM);
-		obj.put("testnet", false);
-		obj.put("keys_n", new Integer(0));
-		obj.put("keys", jsonlist);
-		StringWriter jsonOut = new StringWriter();
-		try {
-			JSONValue.writeJSONString(obj, jsonOut);
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-		String jsonText = jsonOut.toString();
-		
-		//Add pairing data to file
-		JSONParser parser = new JSONParser();
-		Object fileObj = null;
-		try {
-			fileObj = parser.parse(new FileReader(filePath));
-		} catch (IOException | ParseException e) {
-			e.printStackTrace();
-		}
-		
-		
-		JSONObject jsonObject = (JSONObject) obj;
-		
-		try {
-			FileWriter file = new FileWriter(filePath);
-			file.write(jsonText);
-			file.flush();
-			file.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}*/
+		finally{ lock.unlock(); }
 	}
 	
 	public ArrayList<PairingObject> getPairingObjectsArray()
@@ -283,12 +164,14 @@ public class WalletFile extends BASE{
 		JSONParser parser = new JSONParser();
 		Object obj = null;
 		JSONArray jsonArr = null;
+		lock.lock();
 		try {
 			obj = parser.parse(new FileReader(filePath));
 			jsonArr = (JSONArray)obj;
 		} catch (IOException | ParseException e) {
 			e.printStackTrace();
 		}
+		finally{ lock.unlock(); }
 		if(jsonArr.size() > 0)
 		{
 			for(Object o:jsonArr)
@@ -319,16 +202,6 @@ public class WalletFile extends BASE{
 				return o.aes_key;
 		}
 		return "";
-		/*JSONParser parser = new JSONParser();
-		Object obj = null;
-		try {
-			obj = parser.parse(new FileReader(filePath));
-		} catch (IOException | ParseException e) {
-			e.printStackTrace();
-		}
-		JSONObject jsonObject = (JSONObject) obj;
-		String aeskey = (String) jsonObject.get("aes_key");
-		return aeskey;*/
 	}
 	
 	/**Returns the number of key pairs in the wallet*/
@@ -340,16 +213,6 @@ public class WalletFile extends BASE{
 				return o.keys_n;
 		}
 		return 0;
-		/*JSONParser parser = new JSONParser();
-		Object obj = null;
-		try {
-			obj = parser.parse(new FileReader(filePath));
-		} catch (IOException | ParseException e) {
-			e.printStackTrace();
-		}
-		JSONObject jsonObject = (JSONObject) obj;
-		long numkeys = (Long) jsonObject.get("keys_n");
-		return numkeys;*/
 	}
 	
 	/**Returns the Master Public Key and Chaincode as an ArrayList object*/
@@ -365,18 +228,6 @@ public class WalletFile extends BASE{
 			}
 		}
 		return ret;
-		/*ArrayList<String> arr = new ArrayList<String>();
-		JSONParser parser = new JSONParser();
-		Object obj = null;
-		try {
-			obj = parser.parse(new FileReader(filePath));
-		} catch (IOException | ParseException e) {
-			e.printStackTrace();
-		}
-		JSONObject jsonObject = (JSONObject) obj;
-		arr.add((String) jsonObject.get("master_public_key"));
-		arr.add((String) jsonObject.get("chain_code"));
-		return arr;*/
 	}
 	
 	/** Returns a list of all addresses in the wallet*/
@@ -392,25 +243,6 @@ public class WalletFile extends BASE{
 					arr.add(ko.address);
 				}
 		}
-		/*JSONParser parser = new JSONParser();
-		Object obj = null;
-		try {
-			obj = parser.parse(new FileReader(filePath));
-		} catch (IOException | ParseException e) {
-			e.printStackTrace();
-		}
-		JSONObject jsonObject = (JSONObject) obj;
-		JSONArray msg = (JSONArray) jsonObject.get("keys");
-		Iterator<JSONObject> iterator = msg.iterator();
-		JSONArray jsonlist = new JSONArray();
-		while (iterator.hasNext()) {
-			jsonlist.add(iterator.next());
-		}
-		JSONObject jsonAddr = (JSONObject) obj;
-		for(int i=0; i<jsonlist.size(); i++){
-			jsonAddr = (JSONObject) jsonlist.get(i);
-			arr.add((String) jsonAddr.get("address"));
-		}	*/	
 		return arr;
 	}
 	
@@ -428,29 +260,6 @@ public class WalletFile extends BASE{
 			}
 		}
 		return 0;
-		/*ArrayList<String> arr = new ArrayList<String>();
-		JSONParser parser = new JSONParser();
-		Object obj = null;
-		try {
-			obj = parser.parse(new FileReader(filePath));
-		} catch (IOException | ParseException e) {
-			e.printStackTrace();
-		}
-		JSONObject jsonObject = (JSONObject) obj;
-		JSONArray msg = (JSONArray) jsonObject.get("keys");
-		Iterator<JSONObject> iterator = msg.iterator();
-		JSONArray jsonlist = new JSONArray();
-		while (iterator.hasNext()) {
-			jsonlist.add(iterator.next());
-		}
-		JSONObject jsonAddr = (JSONObject) obj;
-		for(int i=0; i<jsonlist.size(); i++){
-			jsonAddr = (JSONObject) jsonlist.get(i);
-			String jaddr = (String) jsonAddr.get("address");
-			long index = (Long) jsonAddr.get("index");
-			if (jaddr.equals(Address)){return index;}	
-		}
-		return 0;*/
 	}
 	
 	/**Returns the private key for a given address*/
@@ -467,29 +276,7 @@ public class WalletFile extends BASE{
 				}
 		}
 		return null;
-		/*ArrayList<String> arr = new ArrayList<String>();
-		JSONParser parser = new JSONParser();
-		Object obj = null;
-		try {
-			obj = parser.parse(new FileReader(filePath));
-		} catch (IOException | ParseException e) {
-			e.printStackTrace();
-		}
-		JSONObject jsonObject = (JSONObject) obj;
-		JSONArray msg = (JSONArray) jsonObject.get("keys");
-		Iterator<JSONObject> iterator = msg.iterator();
-		JSONArray jsonlist = new JSONArray();
-		while (iterator.hasNext()) {
-			jsonlist.add(iterator.next());
-		}
-		JSONObject jsonAddr = (JSONObject) obj;
-		for(int i=0; i<jsonlist.size(); i++){
-			jsonAddr = (JSONObject) jsonlist.get(i);
-			String jaddr = (String) jsonAddr.get("address");
-			String pkey = (String) jsonAddr.get("priv_key");
-			if (jaddr.equals(Address)){return pkey;}	
-		}
-		return null;*/
+		
 	}
 	
 	/**Returns the private key using an index as the input*/
@@ -506,29 +293,6 @@ public class WalletFile extends BASE{
 				}
 		}
 		return null;
-		/*ArrayList<String> arr = new ArrayList<String>();
-		JSONParser parser = new JSONParser();
-		Object obj = null;
-		try {
-			obj = parser.parse(new FileReader(filePath));
-		} catch (IOException | ParseException e) {
-			e.printStackTrace();
-		}
-		JSONObject jsonObject = (JSONObject) obj;
-		JSONArray msg = (JSONArray) jsonObject.get("keys");
-		Iterator<JSONObject> iterator = msg.iterator();
-		JSONArray jsonlist = new JSONArray();
-		while (iterator.hasNext()) {
-			jsonlist.add(iterator.next());
-		}
-		JSONObject jsonAddr = (JSONObject) obj;
-		for(int i=0; i<jsonlist.size(); i++){
-			jsonAddr = (JSONObject) jsonlist.get(i);
-			long jIndex = (Long) jsonAddr.get("index");
-			String pkey = (String) jsonAddr.get("priv_key");
-			if (jIndex==index){return pkey;}	
-		}
-		return null;*/
 	}
 	
 	public Map<String,Boolean> getTestnets(){
@@ -545,16 +309,6 @@ public class WalletFile extends BASE{
 			if(o.pairingID.equals(pairID))
 				return o.GCM;
 		return null;
-		/*JSONParser parser = new JSONParser();
-		Object obj = null;
-		try {
-			obj = parser.parse(new FileReader(filePath));
-		} catch (IOException | ParseException e) {
-			e.printStackTrace();
-		}
-		JSONObject jsonObject = (JSONObject) obj;
-		String GCM = (String) jsonObject.get("GCM");
-		return GCM;*/
 	}
 	
 }
