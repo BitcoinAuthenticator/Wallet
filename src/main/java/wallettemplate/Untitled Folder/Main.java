52d4f02fe91cdcc9f21104b0f829f093bece99a6
cpacia <ctpacia@gmail.com>
2014-06-16 01:27:56 -0400

    included new java files

---
+++ src/main/java/wallettemplate/Untitled Folder/Main.java
@@ -0,0 +1,258 @@
+package wallettemplate;
+
+import authenticator.Authenticator;
+import authenticator.OnAuthenticatoGUIUpdateListener;
+import authenticator.ui_helpers.BAApplication;
+
+import com.aquafx_project.AquaFx;
+import com.google.bitcoin.core.NetworkParameters;
+import com.google.bitcoin.kits.WalletAppKit;
+import com.google.bitcoin.params.MainNetParams;
+import com.google.bitcoin.params.TestNet3Params;
+import com.google.bitcoin.store.BlockStoreException;
+import com.google.bitcoin.utils.BriefLogFormatter;
+import com.google.bitcoin.utils.Threading;
+import com.google.common.base.Throwables;
+
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.stage.WindowEvent;
+import wallettemplate.utils.BaseUI;
+import wallettemplate.utils.GuiUtils;
+import wallettemplate.utils.TextFieldValidator;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import org.controlsfx.control.action.Action;
+import org.controlsfx.dialog.Dialog;
+import org.controlsfx.dialog.Dialogs;
+
+import static wallettemplate.utils.GuiUtils.*;
+
+public class Main extends BAApplication {
+
+	public static String APP_NAME = "BitcoinAuthenticator";
+    public static Main instance;
+    public static Controller mainController;
+    private StackPane uiStack;
+    public AnchorPane root;
+    public static Stage stage;
+    static Stage stg;
+    
+    @Override
+    public void start(Stage mainWindow) throws Exception {
+    	stg = mainWindow;
+        instance = this;
+        // Show the crash dialog for any exceptions that we don't handle and that hit the main loop.
+        GuiUtils.handleCrashesOnThisThread();
+        
+        if(super.BAInit(APP_NAME))
+	        try {
+	            init(mainWindow);
+	        } catch (Throwable t) {
+	            // Nicer message for the case where the block store file is locked.
+	            if (Throwables.getRootCause(t) instanceof BlockStoreException) {
+	                GuiUtils.informationalAlert("Already running", "This application is already running and cannot be started twice.");
+	            } else {
+	                throw t;
+	            }
+	        }
+        else
+        	Runtime.getRuntime().exit(0);
+    }
+
+    private void init(Stage mainWindow) throws Exception {
+        if (System.getProperty("os.name").toLowerCase().contains("mac")) {
+            AquaFx.style();
+        }
+        // Load the GUI. The Controller class will be automagically created and wired up.
+       
+        mainWindow.initStyle(StageStyle.UNDECORATED);
+        URL location = getClass().getResource("GUI.fxml");
+        FXMLLoader loader = new FXMLLoader(location);
+		root = (AnchorPane) loader.load();
+		//uiStack = new StackPane(root);
+        Scene scene = new Scene(root, 850, 483);
+        final String file = TextFieldValidator.class.getResource("GUI.css").toString();
+        scene.getStylesheets().add(file);
+        mainWindow.setScene(scene);
+        mainWindow.show();
+        mainController = loader.getController();
+        // Configure the window with a StackPane so we can overlay things on top of the main UI.
+        //TextFieldValidator.configureScene(scene);   // Add CSS that we need.
+        
+        // Make log output concise.
+        BriefLogFormatter.init();
+        // Tell bitcoinj to run event handlers on the JavaFX UI thread. This keeps things simple and means
+        // we cannot forget to switch threads when adding event handlers. Unfortunately, the DownloadListener
+        // we give to the app kit is currently an exception and runs on a library thread. It'll get fixed in
+        // a future version.
+        Threading.USER_THREAD = Platform::runLater;
+        // Create the app kit. It won't do any heavyweight initialization until after we start it.
+        NetworkParameters np = null;
+        if(this.ApplicationParams.getBitcoinNetworkType() == NetworkType.MAIN_NET){
+        	np = MainNetParams.get();
+        	bitcoin = new WalletAppKit(np, new File("."), APP_NAME);
+            // Checkpoints are block headers that ship inside our app: for a new user, we pick the last header
+            // in the checkpoints file and then download the rest from the network. It makes things much faster.
+            // Checkpoint files are made using the BuildCheckpoints tool and usually we have to download the
+            // last months worth or more (takes a few seconds).
+            bitcoin.setCheckpoints(getClass().getResourceAsStream("checkpoints"));
+            // As an example!
+            bitcoin.useTor();
+        }
+        else if(this.ApplicationParams.getBitcoinNetworkType() == NetworkType.TEST_NET){
+        	np = TestNet3Params.get();
+        	bitcoin = new WalletAppKit(np, new File("."), APP_NAME+"_testnet");
+        	bitcoin.useTor();
+        }        
+
+        // Now configure and start the appkit. This will take a second or two - we could show a temporary splash screen
+        // or progress widget to keep the user engaged whilst we initialise, but we don't.
+        bitcoin.setDownloadListener(mainController.progressBarUpdater())
+               .setBlockingStartup(false)
+               .setUserAgent(APP_NAME, "1.0");
+        bitcoin.startAsync();
+        bitcoin.awaitRunning();
+        // Don't make the user wait for confirmations for now, as the intention is they're sending it their own money!
+        bitcoin.wallet().allowSpendingUnconfirmedTransactions();
+        bitcoin.peerGroup().setMaxConnections(11);
+        System.out.println(bitcoin.wallet());
+                
+        /**
+         * Authenticator Operation Setup
+         */
+        new Authenticator(bitcoin.wallet(), bitcoin.peerGroup(), new OnAuthenticatoGUIUpdateListener(){
+
+			@Override
+			public void simpleTextMessage(String msg) {
+				
+			}
+
+			@Override
+			public void riseAlertToUser(String msg, String title) {
+				
+			}
+        	
+        })
+        .setApplicationParams(ApplicationParams)
+        .start();
+
+        mainController.onBitcoinSetup();
+        stage = mainWindow;
+        mainWindow.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent e) {
+            	Action response = null;
+            	if(Authenticator.getPendingRequestSize() > 0 || Authenticator.operationsQueue.size() > 0){
+            		response = Dialogs.create()
+                	        .owner(stage)
+                	        .title("Warning !")
+                	        .masthead("Pending Requests/ Operations")
+                	        .message("Exiting now will cancell all pending requests and operations.\nDo you want to continue?")
+                	        .actions(Dialog.Actions.YES, Dialog.Actions.NO)
+                	        .showConfirm();
+                	
+            	}
+            	
+            	// Or no conditioning needed or user pressed Ok
+            	if (response == null ||
+            			(response != null && response == Dialog.Actions.YES)) {
+            		bitcoin.stopAsync();
+                    bitcoin.awaitTerminated();
+                    
+                    // Waits until all threads are stopped
+                    try { Authenticator.stop(stage); } catch (InterruptedException e1) { e1.printStackTrace(); }
+                    
+                    // Forcibly terminate the JVM because Orchid likes to spew non-daemon threads everywhere.
+                    Runtime.getRuntime().exit(0);
+            	}
+            	else if(response != null && response == Dialog.Actions.NO)
+            		e.consume();
+            	
+            }
+        });
+    }
+    
+    public class OverlayUI<T> {
+        public Node ui;
+        public T controller;
+
+        public OverlayUI(Node ui, T controller) {
+            this.ui = ui;
+            this.controller = controller;
+        }
+
+        public void show() {
+            blurOut(root);
+            uiStack.getChildren().add(ui);
+            fadeIn(ui);
+        }
+
+        public void done() {
+            checkGuiThread();
+            fadeOutAndRemove(ui, uiStack);
+            blurIn(root);
+            this.ui = null;
+            this.controller = null;
+            mainController.refreshUI();
+        }
+    }
+
+    public <T> OverlayUI<T> overlayUI(Node node, T controller) {
+        checkGuiThread();
+        OverlayUI<T> pair = new OverlayUI<T>(node, controller);
+        // Auto-magically set the overlayUi member, if it's there.
+        try {
+            controller.getClass().getDeclaredField("overlayUi").set(controller, pair);
+        } catch (IllegalAccessException | NoSuchFieldException ignored) {
+        }
+        pair.show();
+        return pair;
+    }
+
+    /** Loads the FXML file with the given name, blurs out the main UI and puts this one on top. */
+    public <T extends BaseUI> OverlayUI<T> overlayUI(String name, ArrayList<Object> params) {
+        try {
+            checkGuiThread();
+            // Load the UI from disk.
+            URL location = getClass().getResource(name);
+            FXMLLoader loader = new FXMLLoader(location);
+            Pane ui = loader.load();
+            T controller = loader.getController();
+            if(params != null && params.size() >0){
+            	controller.setParams(params);
+            	controller.loadParams();
+            }
+            OverlayUI<T> pair = new OverlayUI<T>(ui, controller);
+            // Auto-magically set the overlayUi member, if it's there.
+            try {
+                controller.getClass().getDeclaredField("overlayUi").set(controller, pair);
+            } catch (IllegalAccessException | NoSuchFieldException ignored) {
+            }
+            pair.show();
+            return pair;
+        } catch (IOException e) {
+            throw new RuntimeException(e);  // Can't happen.
+        }
+    }
+
+    @Override
+    public void stop() throws Exception { }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/wallettemplate/Untitled Folder/OneName.java
@@ -0,0 +1,113 @@
+package wallettemplate;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.net.URLConnection;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import javax.imageio.ImageIO;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class OneName {
+
+	/**Downloads the OneName avatar, scales and crops it.*/
+	   public void getOneName(String onename) throws IOException, JSONException {
+	    	//Get the url for the image from the onename json
+	    	JSONObject json = readJsonFromUrl("https://onename.io/" + onename + ".json");
+	    	JSONObject avatar = json.getJSONObject("avatar");
+	    	JSONObject name = json.getJSONObject("name");
+	    	String formattedname = name.getString("formatted");
+	    	String imgURL = avatar.getString("url");
+	    	//Download the image
+	    	BufferedImage image =null;
+	    	URL url =new URL(imgURL);
+	    	try{image = ImageIO.read(url);}
+	    	catch(IOException e){e.printStackTrace();}
+	    	//Scale the image
+	    	int imageWidth  = image.getWidth();
+	        int imageHeight = image.getHeight();
+	        int width;
+	        int height;
+	        if (imageWidth>imageHeight){
+	        	Double temp = (73/ (double)imageHeight)*(double)imageWidth;
+	        	width = temp.intValue();
+	        	height = 73;
+	        } 
+	        else {
+	        	Double temp = (73/ (double) imageWidth)* (double) imageHeight;
+	        	width = 73;
+	        	height = temp.intValue();
+	        }
+	        BufferedImage scaledImage = new BufferedImage(width, height, image.getType());
+	        Graphics2D g = scaledImage.createGraphics();
+	        g.drawImage(image, 0, 0, width, height, null);
+	        g.dispose();
+	        //Crop the image
+	        int x,y;
+	        if (width>height){
+	        	y=0;
+	        	x=(width-73)/2;
+	        }
+	        else {
+	        	x=0;
+	        	y=(height-73)/2;
+	        }
+	        BufferedImage croppedImage = scaledImage.getSubimage(x, y, 73, 73);
+	        //Set image on GUI
+	        Image img = createImage(croppedImage);
+	        Main.mainController.setAvatar(img);
+	        Main.mainController.setName(formattedname);
+	    }
+
+	    /**For reading the JSON*/
+		private static String readAll(Reader rd) throws IOException {
+		    StringBuilder sb = new StringBuilder();
+		    int cp;
+		    while ((cp = rd.read()) != -1) {
+		      sb.append((char) cp);
+		    }
+		    return sb.toString();
+		  }
+
+		/**Reads JSON object from a URL*/
+		public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException { 
+		    URL urladdr = new URL(url);
+	        URLConnection conn = urladdr.openConnection();
+	        conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2");
+	        BufferedReader rd = null;
+		    try {
+		      rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		      String jsonText = readAll(rd);
+		      JSONObject json = new JSONObject(jsonText);
+		      return json;
+		    } finally {
+		      rd.close();
+		    }
+		  }
+		
+		 public static javafx.scene.image.Image createImage(BufferedImage image) throws IOException {
+			    ByteArrayOutputStream out = new ByteArrayOutputStream();
+			    ImageIO.write((RenderedImage) image, "png", out);
+			    out.flush();
+			    ByteArrayInputStream in = new ByteArrayInputStream(out.toByteArray());
+			    return new javafx.scene.image.Image(in);
+			  }
+		 
+}
