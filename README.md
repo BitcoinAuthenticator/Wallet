dacbb34d518281a31e94e43f426b7cd64e02be9c
Alon Muroch <alonmuroch@gmail.com>
2014-07-09 16:21:24 +0200

    updated readme

--- README.md
+++ README.md
@@ -4,11 +4,14 @@ BitcoinAuthenticator - Wallet
 ###IMPORTANT - This project is under developement.
 
 ####What Is This ?
-A GUI bitcoin wallet based on bitcoinj template wallet for the [BitcoinAuthenticator](https://github.com/cpacia/BitcoinAuthenticator) project
+Bitcoin Authenticator is a P2P bitcoin wallet and [android application](https://github.com/cpacia/BitcoinAuthenticator) for creating a 2 factor transaction authentication and authorization. Bitcoin Authenticator is composed of a desktop wallet and an  [android application](https://github.com/cpacia/BitcoinAuthenticator), by pairing the two together they create an P2SH bitcoin address. Any bitcoins that are transferred to that address will require the authentication and digital signature of both the desktop wallet and the android Authenticator app.
 
-#### Requirements
+####How do i get the android application ? 
+[Click here](https://github.com/cpacia/BitcoinAuthenticator) 
+
+#### Building requirements for the BitcoinAuthenticator wallet
 1. Install Java 8
-2. [Eclipse Kepler (4.3.2) SR2 with Java™ 8 Patches](http://www.eclipse.org/downloads/index-java8.php) (Obviously, only if you work with eclipse)
+2. [Eclipse Luna, Support for java 8](http://www.eclipse.org/home/index.php) (Obviously, only if you work with eclipse)
 
 
 
@@ -25,6 +28,20 @@ A GUI bitcoin wallet based on bitcoinj template wallet for the [BitcoinAuthentic
 
 #### Building
 
+#####With Maven
+1. Clone the project:
+ ```
+ $ git clone https://github.com/negedzuregal/BitcoinAuthWallet.git
+ ```
+2. 
+ ```
+ $ cd BitcoinAuthWallet
+ ```
+3. build: 
+ ```
+$mvn clean package
+ ```
+
 ##### Protobuf class
 From the Protobuf folder(/src/main/java/authenticator/protobuf):
  ```
@@ -35,20 +52,15 @@ $ protoc <Proto File>.proto --java_out=../../
  ```
 $ mvn clean compile assembly:single
  ```
-##### OSX Package (dmg)
- ```
-$ mvn install (Doesn't work yet)
- ```
 
 #### TODO
 1. Testing Testing Testing !
-2. Improve UI and make it smoother .. string validation where needed .. you know the drill
-3. Transaction list Pane
-4. Wallet backup - implemented SSS, missing UI + PDF export
-5. Currency conversions from bitcoin
-6. Smart input selection
-7. Smart fee calculation
-8. Improve command line parameters for various functionalities
-9. Basic RPC
-10. mixing
-11. Integrate with [OneName](https://onename.io)
+2. Transaction list Pane
+3. Currency conversions from bitcoin
+4. Smart input selection
+5. Smart fee calculation
+6. Improve command line parameters for various functionalities
+7. Basic RPC
+8. mixing
+9. Integrate with [OneName](https://onename.io)
+10. Discover scheme for the key hierarchy
--- src/test/java/authenticator/SignTransactionTest.java
+++ src/test/java/authenticator/SignTransactionTest.java
@@ -29,17 +29,24 @@ import authenticator.operations.OperationsUtils.SignProtocol;
 import authenticator.operations.OperationsUtils.CommunicationObjects.SignMessage;
 import authenticator.protobuf.AuthWalletHierarchy.HierarchyAddressTypes;
 import authenticator.protobuf.ProtoConfig.ATAddress;
+import authenticator.protobuf.ProtoConfig.PairedAuthenticator;
 
 import com.google.bitcoin.core.Address;
 import com.google.bitcoin.core.AddressFormatException;
 import com.google.bitcoin.core.Coin;
 import com.google.bitcoin.core.ECKey;
 import com.google.bitcoin.core.NetworkParameters;
+import com.google.bitcoin.core.Sha256Hash;
 import com.google.bitcoin.core.Transaction;
 import com.google.bitcoin.core.TransactionInput;
 import com.google.bitcoin.core.TransactionOutput;
 import com.google.bitcoin.core.Utils;
 import com.google.bitcoin.core.Wallet;
+import com.google.bitcoin.crypto.DeterministicKey;
+import com.google.bitcoin.crypto.HDKeyDerivation;
+import com.google.bitcoin.crypto.MnemonicCode;
+import com.google.bitcoin.crypto.MnemonicException.MnemonicLengthException;
+import com.google.bitcoin.crypto.TransactionSignature;
 import com.google.bitcoin.params.MainNetParams;
 import com.google.bitcoin.script.Script;
 import com.google.bitcoin.script.ScriptBuilder;
@@ -47,22 +54,35 @@ import com.google.common.collect.ImmutableList;
 
 public class SignTransactionTest {
 	
-	Transaction tx;
+	
+	//Transaction tx;
 	String pairingID = "1";
-	//SecretKey aes;
 	String aes = "A2F72940109899C1708511B4867727E507299E276B2E44B5D48BBFE8689C17F0";
-	ECKey inPubKey;
-	Address inAddress;
-	ATAddress.Builder bAddress;
+	String authenticatorSeed = "3701720EA7EB2CC7861DC0F0A6A8DB3AAF736C6139CC0F2BF72D410388D360C1E1FE0651B3DB554F51DC8C9093030A17216187E9FFC861A9E970A5EF485AC49F";
+	PairedAuthenticator.Builder po;
 	WalletOperation wallet;
 	
-	private Address getP2SHAddress(){
+	// outputs
+	/*ECKey outKey1;
+	ECKey outKey2;
+	Address outAddress;*/
+	
+	// inputs 
+	/*ECKey inKey;
+	Address inAddress;
+	ATAddress.Builder bAddress;*/
+	
+	private Address getP2SHAddress(DeterministicKey authMKey, int childIndex){
 		//network params
 		NetworkParameters params = MainNetParams.get();
-		ECKey k1 = new ECKey();
-		ECKey k2 = new ECKey();
+		
+		// derive auth child key
+		DeterministicKey childKey = HDKeyDerivation.deriveChildKey(authMKey, childIndex);
+		ECKey outKey1 = new ECKey(childKey.getPrivKeyBytes(), childKey.getPubKey());
+		
+		ECKey outKey2 = new ECKey();
 		//Create a 2-of-2 multisig output script.
-		List<ECKey> keys = ImmutableList.of(k1,k2);//childPubKey, walletKey);
+		List<ECKey> keys = ImmutableList.of(outKey1,outKey2);//childPubKey, walletKey);
 		byte[] scriptpubkey = Script.createMultiSigOutputScript(2,keys);
 		Script script = ScriptBuilder.createP2SHOutputScript(Utils.sha256hash160(scriptpubkey));
 		//Create the address
@@ -70,53 +90,88 @@ public class SignTransactionTest {
 		
 		return multisigaddr;
 	}
-
-	/*private SecretKey getAESKey() throws NoSuchAlgorithmException{
-		//Generate 256 bit key.
-		 KeyGenerator kgen = KeyGenerator.getInstance("AES");
-	     kgen.init(256);	   
-	     SecretKey sharedsecret = kgen.generateKey();
-	     System.out.println(BAUtils.bytesToHex(sharedsecret.getEncoded()));
-	     return sharedsecret;
-	}*/
 	
-	private Transaction preparePayToHashTransaction() throws Exception{
-		Coin outAmpunt = Coin.valueOf(100000000 - 10000);
-		tx = Mockito.mock(Transaction.class);
-		
-		// wallet 
-		pairingID = "1";
-		wallet = Mockito.mock(WalletOperation.class);
-		Mockito.when(wallet.getNetworkParams()).thenReturn(MainNetParams.get());
-		
-		// inputs
-		inPubKey =  ECKey.fromPrivate(BAUtils.hexStringToByteArray("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b85501"));
-		inAddress = inPubKey.toAddress(MainNetParams.get());
-		assertTrue(inAddress.toString().equals("15SoHG382McMfyUT9We4mJou7eRGEVXQQj"));
-		bAddress = ATAddress.newBuilder();
-			  bAddress.setAccountIndex(1);
-			  bAddress.setAddressStr(inAddress.toString());
-			  bAddress.setIsUsed(false);
-			  bAddress.setKeyIndex(1);
-			  bAddress.setType(HierarchyAddressTypes.External);
+	private TransactionOutput addOutPut(Coin amount, DeterministicKey authMKey, int childIndex){
+		Address outAddress = getP2SHAddress(authMKey, childIndex);
 		TransactionOutput out = new TransactionOutput(MainNetParams.get(), 
-					null, 
-					outAmpunt.add(Coin.valueOf(10000)), 
-					inAddress);
+				null, 
+				amount, 
+				outAddress);
+		
+		return out;
+	}
+	
+	private TransactionInput addInput(Coin amount, String privateKeyStringHex, String addressString, int keyIndex) throws Exception{
+		ECKey inKey =  ECKey.fromPrivate(BAUtils.hexStringToByteArray(privateKeyStringHex));
+		Address inAddress = inKey.toAddress(MainNetParams.get());
+		assertTrue(inAddress.toString().equals(addressString));
+		ATAddress.Builder bAddress = ATAddress.newBuilder();
+						  bAddress.setAccountIndex(0);
+						  bAddress.setAddressStr(inAddress.toString());
+						  bAddress.setIsUsed(false);
+						  bAddress.setKeyIndex(keyIndex);
+						  bAddress.setType(HierarchyAddressTypes.External);
+		TransactionOutput in = new TransactionOutput(MainNetParams.get(), 
+																	null, 
+																	amount, 
+																	inAddress);
 		TransactionInput inMock  = Mockito.mock(TransactionInput.class);
-		Mockito.when(inMock.getConnectedOutput()).thenReturn(out);
-		ArrayList<TransactionInput> inputs = new ArrayList<TransactionInput>();
-		inputs.add(inMock);
-		Mockito.when(tx.getInputs()).thenReturn(inputs);
+		Mockito.when(inMock.getConnectedOutput()).thenReturn(in);
+		
 		Mockito.when(wallet.findAddressInAccounts(inAddress.toString())).thenReturn(bAddress.build()); 
 		Mockito.when(wallet.getECKeyFromAccount(bAddress.getAccountIndex(),
 			bAddress.getType(),
-			bAddress.getKeyIndex())).thenReturn(inPubKey);
+			bAddress.getKeyIndex())).thenReturn(inKey);
+		
+		return inMock;
+	}
+	
+	private Transaction preparePayToHashTransaction() throws Exception{
+		Coin outAmpunt = Coin.valueOf(100000000 - 10000);
+		Transaction tx = Mockito.mock(Transaction.class);
+		
+		// general 
+		po = PairedAuthenticator.newBuilder();
+		po.setAesKey(aes);
+		po.setGCM("some gcm");
+		po.setPairingID(pairingID);
+		po.setTestnet(false);
+		
+		pairingID = "1";
+		wallet = Mockito.mock(WalletOperation.class);
+		Mockito.when(wallet.getNetworkParams()).thenReturn(MainNetParams.get());
 		Mockito.when(wallet.getAESKey(pairingID)).thenReturn(aes);
+		
+		// inputs
+		ArrayList<TransactionInput> inputs = new ArrayList<TransactionInput>();
+		TransactionInput inMock = addInput(outAmpunt.add(Coin.valueOf(10000)), 
+											"e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b85501", 
+											"15SoHG382McMfyUT9We4mJou7eRGEVXQQj",
+											1);
+		inputs.add(inMock);
+		Mockito.when(tx.getInputs()).thenReturn(inputs);
+				
+		// outputs
+		ArrayList<TransactionOutput> outputs = new ArrayList<TransactionOutput>();
+		DeterministicKey mPrivKey = HDKeyDerivation.createMasterPrivateKey(BAUtils.hexStringToByteArray(authenticatorSeed));
+		
+		TransactionOutput out = addOutPut(outAmpunt, mPrivKey, 1);
+		outputs.add(out);
+		
+		po.setMasterPublicKey(BAUtils.bytesToHex(mPrivKey.getPubKey()));
+		po.setChainCode(BAUtils.bytesToHex(mPrivKey.getChainCode()));
+		po.setKeysN(1);
+		
+		Mockito.when(tx.getOutputs()).thenReturn(outputs);
+				
+		// mocking for the sign complete
+		//Mockito.doNothing().when(wallet).connectInputs(tx.getInputs());
+		//Mockito.doReturn(null).when(wallet).connectInputs(tx.getInputs());
 		
 		return tx;
 		
 	}
+
 	
 	/**
 	 * Comment out this method for changes in the test
@@ -165,10 +220,41 @@ public class SignTransactionTest {
 	public void prepareTxTest() {
 		byte[] result = null;
 		try {
-			tx = preparePayToHashTransaction();
+			Transaction tx = preparePayToHashTransaction();
 			result = SignProtocol.prepareTX(wallet, tx, pairingID);
 			byte[] expected = BAUtils.hexStringToByteArray("93FBE01C0681D4E88405062FA3C8BEC20414591E006336D8731E643CD055AF750C3A0E478C5E8DC186D57F94FD310C454B40DF676A4531E5AC1B6744BE6DA1142AEB042C048123BD8579570D587CBC95EB2221BAA25FCE2359F61C73F2F2981C791C09D7A3636E67AAC922E4DF7870D32CCBD8AF4552838D24227243C60D2D358FA0768ECE3C025B81AA273ADB98CF92832FCA49E28AFFF7F22542565F5AA409EB785800C022059DA4E02BB66D923FF07E29B44B75E27860F798723760DC2D3E");//prepareTx();
+			// check expected output
 			assertTrue(Arrays.areEqual(result, expected));
+			
+			// Authenticator sig 
+			ArrayList<String> pubKeysArr = new ArrayList<String>(); pubKeysArr.add("02c0f40a12cc1d0abf5a74ee4955c0559e48553469f395c13aaefe0c4ef376bade");
+			ArrayList<Integer> indexArr = new ArrayList<Integer>(); indexArr.add(1);
+			ArrayList<byte[]> sigs = new ArrayList<byte[]>();
+			for (int j=0; j<tx.getInputs().size(); j++){
+				//Derive the private key needed to sign the transaction
+				ArrayList<Integer> index = indexArr;
+				ArrayList<String> walpubkeys = pubKeysArr;
+				HDKeyDerivation HDKey = null;
+				DeterministicKey masterKey = HDKey.createMasterPrivateKey(BAUtils.hexStringToByteArray(authenticatorSeed));
+				DeterministicKey childKey = HDKey.deriveChildKey(masterKey,index.get(j));
+				byte[] privKey = childKey.getPrivKeyBytes();
+				byte[] pubKey = childKey.getPubKey();
+				ECKey authenticatorKey = new ECKey(privKey, pubKey);
+				ECKey walletPubKey = new ECKey(null, BAUtils.hexStringToByteArray(walpubkeys.get(j))); 							
+				List<ECKey> keys = ImmutableList.of(authenticatorKey, walletPubKey);
+				//Create the multisig script we will be using for signing. 
+				Script scriptpubkey = ScriptBuilder.createMultiSigOutputScript(2,keys);
+				//Create the signature.
+				Mockito.when(tx.calculateSignature(j, authenticatorKey, scriptpubkey, Transaction.SigHash.ALL, false)).thenCallRealMethod();
+				Mockito.when(tx.hashForSignature(j, scriptpubkey.getProgram(), Transaction.SigHash.ALL, false)).thenCallRealMethod();
+				Sha256Hash ha = tx.hashForSignature(j, scriptpubkey.getProgram(), Transaction.SigHash.ALL, false);
+				TransactionSignature sig2 = tx.calculateSignature(j, authenticatorKey, scriptpubkey, Transaction.SigHash.ALL, false);
+				byte[] signature = sig2.encodeToBitcoin();
+				sigs.add(signature);
+			}
+			
+			SignProtocol.complete(wallet, tx, sigs, po.build());
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 			assertTrue(false);
