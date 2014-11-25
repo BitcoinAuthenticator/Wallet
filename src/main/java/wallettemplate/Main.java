758c2ce9b9f79a09ea33c80e64e1b85b06ee5767
alon muroch <alonmuroch@gmail.com>
2014-11-25 09:50:56 +0200

    fix to previous

--- src/main/java/wallettemplate/Main.java
+++ src/main/java/wallettemplate/Main.java
@@ -491,6 +491,7 @@ public class Main extends BAApplication {
 		String os = System.getProperty("os.name").toLowerCase();
 	    if(os.indexOf("nix") >= 0 || os.indexOf("nux") >= 0) {
 	    	realStart(mainWindow);
+	    	return;
 	    }
 		
     	/**
